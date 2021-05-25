ab37771927ba71a495d41bde2f27dec22aea79ed
Влад Верожинский <vladyslav.verozhynskyi.itis.2020@lpnu.ua>
2021-05-25 16:31:50 +0300

    new

---
+++ labaVlad/FloatPower.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labaVlad
+{
+    public class FloatPower
+    {
+        private float first;
+
+        public float First
+        {
+            get { return first; }
+            set { first = value; }
+        }
+
+        private float second;
+
+        public float Second
+        {
+            get { return second; }
+            set { second = value; }
+        }
+
+
+        public FloatPower(float first, float second)
+        {
+            First = first;
+            Second = second;
+        }
+
+        public double Power()
+        {
+            try
+            {
+                if (Second == 0 || First == 0)
+                {
+                    throw new BadException("Error. Second or First == 0!");
+                }
+                return Math.Pow(First, Second);
+            }
+            catch (BadException e)
+            {
+                Console.WriteLine(e.Message);
+                return 0;
+            }
+            
+        }
+
+        public static FloatPower operator ++(FloatPower floatPower)
+        {
+            return new FloatPower(floatPower.first, floatPower.second + 1);
+        }
+
+        public static FloatPower operator --(FloatPower floatPower)
+        {
+            return new FloatPower(floatPower.first, floatPower.second - 1);
+        }
+
+    }
+}
---
+++ labaVlad/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labaVlad
+{
+    class Program
+    {
+
+        public static FloatPower Init(float first, float second)
+        {
+            return new FloatPower(first, second);
+        }
+
+        public static void Read()
+        {
+            Console.Write("Enter first value: ");
+            float.TryParse(Console.ReadLine(), out float first);
+            Console.Write("Enter second value: ");
+            float.TryParse(Console.ReadLine(), out float second);
+
+            Display(Init(first, second));
+        }
+
+        public static void Display(FloatPower floatPower)
+        {
+            if (floatPower != null)
+            {
+                Console.WriteLine(floatPower.Power());
+                floatPower--;
+                Console.WriteLine(floatPower.Power());
+                floatPower++;
+                Console.WriteLine(floatPower.Power());
+            }
+            else
+            {
+                Console.WriteLine("Введены неверные значения!");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Read();
+        }
+    }
+}
